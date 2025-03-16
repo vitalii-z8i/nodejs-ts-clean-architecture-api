5e5f56c0969339ef46bd55b969963e01a0b3a2eb
vtl <vitalii.zdanovskyi@gmail.com>
2025-03-16 21:08:37 +0200

    Added ESLint & Prettier

---
+++ eslint.config.mjs
@@ -0,0 +1,19 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { files: ["**/*.{ts|js|json}"] },
+  {
+    ignores: [
+      "src/infrastructure/data-access/mysql/migrations/*.js",
+      "dist/**/*",
+    ],
+  },
+  { languageOptions: { globals: globals.node } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  eslintPluginPrettierRecommended,
+];
